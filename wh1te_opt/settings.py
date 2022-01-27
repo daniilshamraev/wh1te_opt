30db6579953dbfd71f2847de3934606f1cfd6a26
Daniil Shamraev <46665117+daniilshamraev@users.noreply.github.com>
2022-01-27 22:38:46 +0300

    Update settings.py

--- wh1te_opt/settings.py
+++ wh1te_opt/settings.py
@@ -19,7 +19,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-g9#efv!j+3cmwmwe!)kjgi19#-z61trczkg&yl=c=jk_3!x&&='
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
@@ -102,12 +102,6 @@ WSGI_APPLICATION = 'wh1te_opt.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
 
 DATABASES = {
     'default': {
